cecd0dfe530354b090f88f7f4605ba9f8e950684
XzoRit <xzorit@hotmail.com>
2021-04-22 19:01:17 +0200

    moved clojure solution into a lein project

--- clj/hackerrank/test/hackerrank/core_test.clj
+++ clj/hackerrank/test/hackerrank/core_test.clj
@@ -1,11 +1,7 @@
-(ns xzr
-  (:use clojure.test))
+(ns hackerrank.core-test
+  (:require [clojure.test :refer :all]
+            [hackerrank.core :refer :all]))
 
-(defn list-replication [num lst]
-    (->>
-        (map #(repeat num %) lst)
-        (flatten)))
-                
 (deftest test-list-replication
     (is (= () (list-replication 0 ())))
     (is (= () (list-replication 9 ())))
@@ -13,32 +9,20 @@
     (is (= '(1 22 333) (list-replication 1 '(1 22 333))))
     (is (= '(1 1 1 22 22 22 333 333 333) (list-replication 3 '(1 22 333)))))
 
-(defn filter-array [delim lst]
-    (for [x lst :when (< x delim)] x))
-  
 (deftest test-filter-array
     (is (= () (filter-array 5 ())))
     (is (= '(0) (filter-array 1 '(0))))
     (is (= '(0) (filter-array 1 '(0 1))))
     (is (= '(4 3) (filter-array 5 '(4 7 5 3)))))
 
-(defn filter-position [lst]
-    (for [a (range (count lst)) :when (odd? a)]
-        (nth lst a)))
-  
 (deftest test-filter-position
     (is (= () (filter-position ())))
     (is (= () (filter-position '(0))))
     (is (= '(1) (filter-position '(0 1))))
     (is (= '(5 4 7 8) (filter-position '(2 5 3 4 6 7 9 8)))))
 
-(defn array-of-n-elements [n]
-    (range n))
-
 (deftest test-array-of-n-elements
     (is (= () (array-of-n-elements 0)))
     (is (= '(0) (array-of-n-elements 1)))
     (is (= '(0 1 2) (array-of-n-elements 3)))
     (is (= '(0 1 2 3 4 5 6 7 8 9) (array-of-n-elements 10))))
-
-(run-tests 'xzr)
